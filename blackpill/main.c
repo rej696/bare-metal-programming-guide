5e736f3b25b284c5be7888b6a9d8bca3defc2b56
rej696 <55083730+rej696@users.noreply.github.com>
2024-02-26 21:20:24 +0000

    work through minimal main and link files

--- blackpill/main.c
+++ blackpill/main.c
@@ -1,9 +1,28 @@
+#include <stdint.h>
+
+int main(void) {
+    return 0;
+}
 
 /* Startup code */
 __attribute__((naked, noreturn)) void _reset(void) {
-  for (;;) {
-    asm("nop");
-  };
+    /* declare linkerscript symbols */
+    extern uint32_t _sbss, _ebss; /* Start/end of .bss section */
+    extern uint32_t _sdata, _edata; /* Start/end of .data section in flash */
+    extern uint32_t _sidata; /* Start of .data section in sram */
+
+    /* set .bss to zero */
+    for (uint32_t *dst = &_sbss; dst < &_ebss; ++dst) {
+        *dst = 0U;
+    }
+
+    for (uint32_t *dst = &_sdata, *src = &_sidata; dst < &_edata; ++dst, ++src) {
+        *dst = *src;
+    }
+
+    main(); /* call main */
+
+    for (;;) (void) 0; /* Infinite loop if main returns */
 }
 
 /* Stack pointer register */
